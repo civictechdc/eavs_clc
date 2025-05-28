3d123b748c27681077520232bfddb9892e698ac7
Jay Qi <jayqi@users.noreply.github.com>
2025-05-28 19:26:05 -0400

    Rename

--- eavs/clean.py
+++ eavs/clean.py
@@ -4,8 +4,9 @@ import pandas as pd
 import pyarrow as pa
 from pandera.io import from_yaml
 from yaml import safe_load
+from loguru import logger
 
-from eavs.config import RAW_DATA_DIR
+from eavs.config import RAW_DATA_DIR, INTERIM_DATA_DIR, PROCESSED_DATA_DIR
 
 COLUMN_METADATA_PATH = Path(__file__).parent / "assets" / "columns.yaml"
 
@@ -16,8 +17,16 @@ def load_column_metadata():
 
     return {(dataset["year"], dataset["version"]): dataset["columns"] for dataset in data}
 
+PROCESSING_FNS: dict[int, callable] = {}
 
-def process_2022():
+def register_cleaning_function(year):
+    def decorator(func):
+        PROCESSING_FNS[year] = func
+        return func
+    return decorator
+
+@register_cleaning_function(2022)
+def clean_2022():
     metadata = load_column_metadata()[(2022, "1.1")]
     # Rename columns
     dtypes = {col["raw_name"]: f"{col["dtype"]}[pyarrow]" for col in metadata}
@@ -42,7 +51,8 @@ def process_2022():
     df_out = df.loc[:, mapping.keys()].rename(columns=mapping)
     return df_out
 
-def process_2020():
+@register_cleaning_function(2020)
+def clean_2020():
     metadata = load_column_metadata()[(2020, "1.2")]
     # Rename columns
     dtypes = {col["raw_name"]: f"{col["dtype"]}[pyarrow]" for col in metadata}
@@ -69,18 +79,22 @@ def process_2020():
 
 
 def main():
-    processed_df = process_2022()
 
-    # Validate schema
     schema = from_yaml(Path(__file__).parent / "assets" / "processed_schema.yaml")
-    schema.validate(processed_df)
-    print(processed_df.head())
 
-    processed_df2 = process_2020()
-    schema = from_yaml(Path(__file__).parent / "assets" / "processed_schema.yaml")
-    schema.validate(processed_df2)
-    print(processed_df2.head())
+    out = {}
+    for year, fn in PROCESSING_FNS.items():
+        logger.info(f"Processing data for {year}")
+        cleaned_df = fn()
+        schema.validate(cleaned_df)
 
+        # Write out intermediate
+        interim_output_path = INTERIM_DATA_DIR / f"{year}.csv"
+        cleaned_df.to_csv(interim_output_path, index=False)
+        out[year] = cleaned_df
+
+    concat_df = pd.concat(out, keys=out.keys()).droplevel(1)
+    concat_df.to_csv(PROCESSED_DATA_DIR / "eavs_cleaned.csv", index=False)
 
 if __name__ == "__main__":
     main()
