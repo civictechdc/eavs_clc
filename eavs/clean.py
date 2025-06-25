daabd386ea9783fad49cfd07ff93ed09351061f2
Jay Qi <2721979+jayqi@users.noreply.github.com>
2025-06-25 19:14:05 -0400

    Clean raw data (A columns) (#18)
    * WIP
    
    * Better names, add 2020
    
    * Rename
    
    * Formatting
    
    * Separate column mappings to separate files; add parquet outputs
    
    ---------
    
    Co-authored-by: Jay Qi <jayqi@users.noreply.github.com>

---
+++ eavs/clean.py
@@ -0,0 +1,108 @@
+from pathlib import Path
+
+from loguru import logger
+import pandas as pd
+from pandera.io import from_yaml
+import pyarrow as pa
+from yaml import safe_load
+
+from eavs.config import CLEANED_DATA_DIR, RAW_DATA_DIR
+
+COLUMN_METADATA_DIR = Path(__file__).parent / "assets" / "column_mappings"
+
+def load_column_mapping(year: int, version: str) -> dict:
+    with (COLUMN_METADATA_DIR / f"{year}.yaml").open("r") as f:
+        data = safe_load(f)
+    for dataset in data:
+        if dataset["version"] == version:
+            return dataset["columns"]
+
+
+PROCESSING_FNS: dict[int, callable] = {}
+
+
+def register_cleaning_function(year):
+    def decorator(func):
+        PROCESSING_FNS[year] = func
+        return func
+
+    return decorator
+
+
+@register_cleaning_function(2022)
+def clean_2022():
+    metadata = load_column_mapping(2022, "1.1")
+    # Rename columns
+    dtypes = {col["raw_name"]: f"{col['dtype']}[pyarrow]" for col in metadata}
+
+    mapping = {col["raw_name"]: col["name"] for col in metadata}
+
+    df = pd.read_excel(
+        RAW_DATA_DIR / "2022" / "1.1" / "2022_EAVS_for_Public_Release_V1.1.xlsx",
+        engine="calamine",
+        dtype_backend="pyarrow",
+        dtype=dtypes,
+        na_values=["Does not apply", "Data not available", "Valid skip"],
+    )
+
+    ## Temporary hack for weird bug in pandas
+    # https://github.com/pandas-dev/pandas/issues/61496
+    for col in dtypes:
+        if dtypes[col] == "string[pyarrow]":
+            df[col] = df[col].astype(pd.ArrowDtype(pa.string()))
+    ##
+
+    df_out = df.loc[:, mapping.keys()].rename(columns=mapping)
+    return df_out
+
+
+@register_cleaning_function(2020)
+def clean_2020():
+    metadata = load_column_mapping(2020, "1.2")
+
+    # Rename columns
+    dtypes = {col["raw_name"]: f"{col['dtype']}[pyarrow]" for col in metadata}
+
+    mapping = {col["raw_name"]: col["name"] for col in metadata}
+
+    df = pd.read_excel(
+        RAW_DATA_DIR / "2020" / "1.2" / "2020_EAVS_for_Public_Release_V1.2.xlsx",
+        engine="calamine",
+        dtype_backend="pyarrow",
+        dtype=dtypes,
+        na_values=["Does not apply", "Data not available", "Valid skip"],
+    )
+
+    ## Temporary hack for weird bug in pandas
+    # https://github.com/pandas-dev/pandas/issues/61496
+    for col in dtypes:
+        if dtypes[col] == "string[pyarrow]":
+            df[col] = df[col].astype(pd.ArrowDtype(pa.string()))
+    ##
+
+    df_out = df.loc[:, mapping.keys()].rename(columns=mapping)
+    return df_out
+
+
+def main():
+    schema = from_yaml(Path(__file__).parent / "assets" / "processed_schema.yaml")
+
+    out = {}
+    for year, fn in PROCESSING_FNS.items():
+        logger.info(f"Processing data for {year}")
+        cleaned_df = fn()
+        schema.validate(cleaned_df)
+
+        # Write out intermediate
+        for ext in ("csv", "parquet"):
+            interim_output_path = CLEANED_DATA_DIR / f"{year}.{ext}"
+            cleaned_df.to_csv(interim_output_path, index=False)
+            out[year] = cleaned_df
+
+    for ext in ("csv", "parquet"):
+        concat_df = pd.concat(out, keys=out.keys()).droplevel(1)
+        concat_df.to_csv(CLEANED_DATA_DIR / f"combined.{ext}", index=False)
+
+
+if __name__ == "__main__":
+    main()
--- eavs/config.py
+++ eavs/config.py
@@ -12,14 +12,8 @@ logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
-INTERIM_DATA_DIR = DATA_DIR / "interim"
-PROCESSED_DATA_DIR = DATA_DIR / "processed"
-EXTERNAL_DATA_DIR = DATA_DIR / "external"
-
-MODELS_DIR = PROJ_ROOT / "models"
-
-REPORTS_DIR = PROJ_ROOT / "reports"
-FIGURES_DIR = REPORTS_DIR / "figures"
+CLEANED_DATA_DIR = DATA_DIR / "cleaned"
+ENRICHED_DATA_DIR = DATA_DIR / "enriched"
 
 # If tqdm is installed, configure loguru with tqdm.write
 # https://github.com/Delgan/loguru/issues/135
